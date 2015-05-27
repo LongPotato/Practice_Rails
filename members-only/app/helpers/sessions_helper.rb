2ec613e6c4010f43eecf6c816961fa8e27f1dd84
Khanh Nguyen <baokhanh_n@hotmail.com>
2015-05-26 17:47:41 -0700

    Add comment

--- members-only/app/helpers/sessions_helper.rb
+++ members-only/app/helpers/sessions_helper.rb
@@ -3,14 +3,14 @@ module SessionsHelper
     remember_token = User.new_token   #give user a new token
     cookies.permanent[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.encript_token(remember_token))
-    current_user = user
+    @current_user = user
   end
 
   def current_user=(user)
     @current_user = user
   end
 
-  #retrieve current_user for every cycle
+  #retrieve current_user for each request
   def current_user
   	remember_token = User.encript_token(cookies[:remember_token])   #retrieve stored token
   	@current_user ||= User.find_by(remember_token: remember_token)
@@ -21,6 +21,7 @@ module SessionsHelper
   end
 
   def sign_out
+  	#because Rails create new instance of controller for each request, we cant new use @current_user
     current_user.update_attribute(:remember_token, User.encript_token(User.new_token))   #change current token to new token
     cookies.delete(:remember_token)
     current_user = nil
